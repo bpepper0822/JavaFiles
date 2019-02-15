738c8910a180f7d2bb0f69da54ccef7108b31803
bpepper0822 <bpeppermail@gmail.com>
2019-02-15 13:32:08 -0600

    Update IceCream.java

--- IceCream/src/icecream/IceCream.java
+++ IceCream/src/icecream/IceCream.java
@@ -5,6 +5,8 @@
  */
 package icecream;
 
+import icecream.TextIO;
+
 /**
  *
  * @author Studio
@@ -15,11 +17,25 @@ public class IceCream {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int counter = 0;
+        int strawberryCounter = 0;
+        int totalCounter = 0;
+        String flavor;
+
         TextIO.putln("test");
         TextIO.readFile("src/icecream/icecream.dat");
-        
-        TextIO.putln(TextIO.getln());
+        while ( TextIO.eof() == false) {
+            flavor = TextIO.getln();
+            totalCounter++;
+            if (flavor == "Strawberry"){
+                strawberryCounter++;
+            }    
+        }
+
+        double percentage = 100 * (strawberryCounter/totalCounter);
+
+        TextIO.putln("There were " + totalCounter + " ice cream cones sold, " + strawberryCounter + " of which were Strawberry.");
+
+        TextIO.putln("Therefore " + percentage + " percent of ice cream cones sold were Strawberry.");
         
     }
     
