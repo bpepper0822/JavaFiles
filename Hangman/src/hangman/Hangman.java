79bd7c0cccda6f4ec820e95f199084532989e74d
bpepper0822 <bpeppermail@gmail.com>
2019-03-02 19:54:43 -0600

    3/2

---
+++ Hangman/src/hangman/Hangman.java
@@ -0,0 +1,34 @@
+package hangman;
+import java.util.*;
+/**
+ *
+ * @author Studio
+ */
+public class Hangman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int turnsLeft = 10;     // Number of guesses
+        
+        int mysteryLetters = 0;
+        String secretWord;
+        String categoryHint;
+        
+        List<Character> guesses = new ArrayList<Character>();
+        
+        while (turnsLeft > 0) {
+            for (char : secretWord){
+                
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
